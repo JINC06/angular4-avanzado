5afefc79ad8c717f47ea23396d7dc71390fb2872
JINC06 <jinc06@hotmail.com>
2018-03-06 06:28:14 -0700

    Menu

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-///Componentes
+// Componentes
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { AnimalsComponent } from './components/animals/animals.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -9,13 +9,14 @@ import { HomeComponent } from './components/home/home.component';
 import { KeepersComponent } from './components/keepers/keepers.component';
 
 const appRoutes: Routes = [
-    //{ path: '', component: HomeComponent },
+    // { path: '', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full'  },
     { path: 'home', component: HomeComponent },
     { path: 'animales', component: AnimalsComponent },
     { path: 'contacto', component: ContactComponent },
     { path: 'cuidadores', component: KeepersComponent },
-    { path: '**', component:HomeComponent }
+    { path: 'tienda', component: TiendaComponent },
+    { path: '**', component: HomeComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
